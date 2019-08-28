4bf262df2e642cc1b97f724b9411e757aabde9ff
Thomas Edwards <thce28@gmail.com>
2019-08-28 13:53:31 +0100

    prisma and database working

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 // 2
 const resolvers = {
     Query: {
         info: () => 'This is the API of a Hackernews Clone',
-        feed: () => links,
+        feed: (root, args, context, info) => {
+            return context.prisma.links()
+        },
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
+        post: (root, args, context) => {
+            return context.prisma.createLink({
                 url: args.url,
-            }
-            links.push(link)
-            return link
+                description: args.description,
+            })
         }
     },
     // Link: {
@@ -36,6 +29,7 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: {prisma}
 })
 server.start(() => console.log('Server is up and running on http://localhost:4000'))
 
