38d273b34ceb2c44f2a22a2f532cde1f3b585bb5
Thomas Edwards <thce28@gmail.com>
2019-08-28 12:49:49 +0100

    pre-database / prisma

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+// 2
+const resolvers = {
+    Query: {
+        info: () => 'This is the API of a Hackernews Clone',
+        feed: () => links,
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
+    },
+    // Link: {
+    //     id: (parent) => parent.id,
+    //     description: (parent) => parent.description,
+    //     url: (parent) => parent.url,
+    // }
+}
+
+// 3
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+})
+server.start(() => console.log('Server is up and running on http://localhost:4000'))
+
+
